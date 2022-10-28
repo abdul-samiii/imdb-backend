da0c3c532b152495b4f62ea845436cd1333fb56f
abdul-samiii <abdulsami.ahmed@devsinc.com>
2022-10-28 23:52:20 +0500

    Trailer add with video

--- controllers/EventController.ts
+++ controllers/EventController.ts
@@ -2,7 +2,19 @@ import { Request, Response, NextFunction } from 'express'
 
 import { Channel, Event } from '../models'
 
-// CREATE CHANNEL
+// CREATE EVENT
 export const AddEvent = async (req: Request, res: Response, next: NextFunction) => {
-  //To be continued
+  const { title, description, img, channel } = req.body
+  const channelExist = await Channel.find({channel})
+  if (channelExist[0] != null) {
+        Event.create({
+          title: title,
+          description: description,
+          img: img,
+          channel: channel
+        })
+        return res.status(200).json({"message": "Event added Successfully"})
+      } else {
+        return res.status(403).json({"message": "Something went wrong"})
+      }
 }
